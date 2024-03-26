228cce210be217458248f96b6ca47c5f9c1a5dac
Praveenasai <150107728+Praveenasai@users.noreply.github.com>
2024-03-26 14:55:12 +0530

    Add files via upload

---
+++ diagonal.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            if(a[i]!=a[j])
+            {
+                a[i][j]=0;
+            }
+            printf("%d",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
